2fc34dafe262c2f404b3564397abf188b4c957c2
pmta <pmta.net@gmail.com>
2023-10-06 19:33:41 +0300

    Restructure in class, end screen with quit and new game

--- snake/snake.py
+++ snake/snake.py
@@ -1,110 +1,166 @@
 import pygame
 import random
 
-# Initialize the game
-pygame.init()
+class SnakeGame:
+    def __init__(self):
+        # Initialize the game
+        pygame.init()
 
-# Set the size of the game window
-window_width = 400
-window_height = 400
+        # Set the size of the game window
+        self.window_width = 400
+        self.window_height = 400
 
-# Set the size of each cell in the grid
-cell_size = 20
+        # Set the size of each cell in the grid
+        self.cell_size = 20
 
-# Set the number of cells in the grid
-grid_width = window_width // cell_size
-grid_height = window_height // cell_size
+        # Set the number of cells in the grid
+        self.grid_width = self.window_width // self.cell_size
+        self.grid_height = self.window_height // self.cell_size
 
-# Set the initial position and length of the snake
-snake_start = (grid_width // 2, grid_height // 2)
-snake = [snake_start, (snake_start[0]-1, snake_start[1]), (snake_start[0]-2, snake_start[1])]
-snake_length = len(snake)
+        # Set the initial position and length of the snake
+        self.snake_start = (self.grid_width // 2, self.grid_height // 2)
+        self.snake = [self.snake_start, (self.snake_start[0] - 1, self.snake_start[1]),
+                      (self.snake_start[0] - 2, self.snake_start[1])]
+        self.snake_length = len(self.snake)
 
-# Set the initial position of the apple
-apple_pos = (random.randint(0, grid_width-1), random.randint(0, grid_height-1))
+        # Set the initial position of the apple
+        self.apple_pos = (random.randint(0, self.grid_width - 1), random.randint(0, self.grid_height - 1))
 
-# Set the initial score
-score = 0
+        # Set the initial score
+        self.score = 0
 
-# Set the initial direction of the snake
-snake_direction = "right"
+        # Set the initial direction of the snake
+        self.snake_direction = "right"
 
-# Set the game window
-game_window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Snake Game")
+        # Set the game window
+        self.game_window = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption("Snake Game")
 
-# Set the game clock
-clock = pygame.time.Clock()
+        # Set the game clock
+        self.clock = pygame.time.Clock()
 
-# Main game loop
-game_over = False
-while not game_over:
-    # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and snake_direction != "down":
-                snake_direction = "up"
-            elif event.key == pygame.K_DOWN and snake_direction != "up":
-                snake_direction = "down"
-            elif event.key == pygame.K_LEFT and snake_direction != "right":
-                snake_direction = "left"
-            elif event.key == pygame.K_RIGHT and snake_direction != "left":
-                snake_direction = "right"
+        # Set the font
+        self.font = pygame.font.Font(None, 24)
 
-    # Update snake position
-    snake_head = snake[0]
-    if snake_direction == "up":
-        new_head = (snake_head[0], snake_head[1]-1)
-    elif snake_direction == "down":
-        new_head = (snake_head[0], snake_head[1]+1)
-    elif snake_direction == "left":
-        new_head = (snake_head[0]-1, snake_head[1])
-    elif snake_direction == "right":
-        new_head = (snake_head[0]+1, snake_head[1])
+        # Set the button colors
+        self.button_color = (0, 255, 0)
+        self.button_hover_color = (0, 200, 0)
 
-    snake.insert(0, new_head)
+        # Set the game state
+        self.game_active = True
 
-    # Check if snake hits apple
-    if snake[0] == apple_pos:
-        # Increase snake length and score
-        snake_length += 1
-        score += 100
-        # Generate new apple position
-        apple_pos = (random.randint(0, grid_width-1), random.randint(0, grid_height-1))
+    def show_end_screen(self):
+        # Display the game over text
+        game_over_text = self.font.render("Game Over", True, (255, 255, 255))
+        self.game_window.blit(game_over_text, (self.window_width // 2 - game_over_text.get_width() // 2,
+                                                self.window_height // 2 - game_over_text.get_height() // 2))
 
-    # Check if snake hits wall or itself
-    if (snake[0][0] < 0 or snake[0][0] >= grid_width or
-        snake[0][1] < 0 or snake[0][1] >= grid_height or
-        snake[0] in snake[1:]):
-        game_over = True
+        # Create the new game button
+        new_game_button = pygame.Rect(self.window_width // 2 - 100, self.window_height // 2 + 50, 100, 50)
+        pygame.draw.rect(self.game_window, self.button_color, new_game_button)
+        new_game_text = self.font.render("New Game", True, (255, 255, 255))
+        new_game_text_rect = new_game_text.get_rect()
+        new_game_text_rect.center = new_game_button.center
+        self.game_window.blit(new_game_text, new_game_text_rect)
 
-    # Update snake length
-    if len(snake) > snake_length:
-        snake.pop()
+        # Create the quit button
+        quit_button = pygame.Rect(self.window_width // 2 + 50, self.window_height // 2 + 50, 100, 50)
+        pygame.draw.rect(self.game_window, self.button_color, quit_button)
+        quit_text = self.font.render("Quit", True, (255, 255, 255))
+        quit_text_rect = quit_text.get_rect()
+        quit_text_rect.center = quit_button.center
+        self.game_window.blit(quit_text, quit_text_rect)
 
-    # Render the game window
-    game_window.fill((0, 0, 0))
+        # Check for mouse button clicks
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                if new_game_button.collidepoint(mouse_pos):
+                    # Start a new game loop
+                    self.__init__()
+                    self.game_loop()
+                elif quit_button.collidepoint(mouse_pos):
+                    pygame.quit()
+                    quit()
 
-    # Draw the snake
-    for pos in snake:
-        pygame.draw.rect(game_window, (0, 255, 0), (pos[0]*cell_size, pos[1]*cell_size, cell_size, cell_size))
 
-    # Draw the apple
-    pygame.draw.rect(game_window, (255, 0, 0), (apple_pos[0]*cell_size, apple_pos[1]*cell_size, cell_size, cell_size))
 
-    # Draw the score
-    font = pygame.font.Font(None, 24)
-    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
-    game_window.blit(score_text, (10, 10))
+    def game_loop(self):
+        # Main game loop
+        while True:
+            if self.game_active:
+                # Event handling
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP and self.snake_direction != "down":
+                            self.snake_direction = "up"
+                        elif event.key == pygame.K_DOWN and self.snake_direction != "up":
+                            self.snake_direction = "down"
+                        elif event.key == pygame.K_LEFT and self.snake_direction != "right":
+                            self.snake_direction = "left"
+                        elif event.key == pygame.K_RIGHT and self.snake_direction != "left":
+                            self.snake_direction = "right"
 
-    # Update the game display
-    pygame.display.flip()
+                # Update snake position
+                snake_head = self.snake[0]
+                if self.snake_direction == "up":
+                    new_head = (snake_head[0], snake_head[1] - 1)
+                elif self.snake_direction == "down":
+                    new_head = (snake_head[0], snake_head[1] + 1)
+                elif self.snake_direction == "left":
+                    new_head = (snake_head[0] - 1, snake_head[1])
+                elif self.snake_direction == "right":
+                    new_head = (snake_head[0] + 1, snake_head[1])
 
-    # Set the game FPS
-    clock.tick(10)
+                self.snake.insert(0, new_head)
 
-# Quit the game
-pygame.quit()
+                # Check if snake hits apple
+                if self.snake[0] == self.apple_pos:
+                    # Increase snake length and score
+                    self.snake_length += 1
+                    self.score += 100
+                    # Generate new apple position
+                    self.apple_pos = (random.randint(0, self.grid_width - 1), random.randint(0, self.grid_height - 1))
 
+                # Check if snake hits wall or itself
+                if (self.snake[0][0] < 0 or self.snake[0][0] >= self.grid_width or
+                        self.snake[0][1] < 0 or self.snake[0][1] >= self.grid_height or
+                        self.snake[0] in self.snake[1:]):
+                    self.game_active = False
+
+                # Update snake length
+                if len(self.snake) > self.snake_length:
+                    self.snake.pop()
+
+                # Render the game window
+                self.game_window.fill((0, 0, 0))
+
+                # Draw the snake
+                for pos in self.snake:
+                    pygame.draw.rect(self.game_window, (0, 255, 0),
+                                     (pos[0] * self.cell_size, pos[1] * self.cell_size, self.cell_size, self.cell_size))
+
+                # Draw the apple
+                pygame.draw.rect(self.game_window, (255, 0, 0),
+                                 (self.apple_pos[0] * self.cell_size, self.apple_pos[1] * self.cell_size,
+                                  self.cell_size, self.cell_size))
+
+                # Draw the score
+                score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+                self.game_window.blit(score_text, (10, 10))
+            else:
+                self.show_end_screen()
+
+            # Update the game display
+            pygame.display.flip()
+
+            # Set the game FPS
+            self.clock.tick(10)
+
+# Call the game loop function from __main__
+if __name__ == "__main__":
+    game = SnakeGame()
+    game.game_loop()
